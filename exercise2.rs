a171cd5a92034a1109a81ef09aeaa9427afee3cb
xuanbao2008 <xuanbao2008@gmail.com>
2022-06-13 19:54:05 +0900

    update

--- exercise2.rs
+++ exercise2.rs
@@ -1,14 +1,11 @@
 use std::fs;
 use std::io;
 fn main() {
-    // Input String outside
-    let contents = fs::read_to_string("word-list.txt").expect("Something went wrong");
-    // Input your key WORD
-    println!("Please input your WORD :");
-    let mut input_str = String::new();
-            io::stdin().read_line(&mut input_str).unwrap();
-    println!("Your searching Key-Word : {}", &input_str);
-    //Return the number of repeat
-    let abc = contents.matches(&input_str.trim()).count();
-    println!("The number of repeat : {} ", abc);
+  let contents = fs::read_to_string("word-list.txt").expect("Something went wrong");
+  println!("Please input keyword:");
+  let mut input_str = String::new();
+  io::stdin().read_line(&mut input_str).unwrap();
+  println!("Your searching keyword: {}", &input_str);
+  let matched_word = contents.matches(&input_str.trim()).count();
+  println!("The number of repeat: {} ", matched_word);
 }
